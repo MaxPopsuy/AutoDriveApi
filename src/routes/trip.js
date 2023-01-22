cb176bcafd458428ddc6943f84b971b3085bfb64
Max Popsuy <maxim09p@gmail.com>
2023-01-22 12:23:30 +0200

    22.01.23: minor fix

--- src/routes/trip.js
+++ src/routes/trip.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", trip.getAll);
 router.get("/trip/:id", trip.getById);
-router.get("/find", trip.find);
+router.post("/find", trip.find);
 router.post("/create", trip.create);
 router.patch("/driver/:id", trip.setDriverRating);
 router.patch("/passenger/:id", trip.setPassengerRating);
